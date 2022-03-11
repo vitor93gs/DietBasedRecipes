9b140a3643f8d50e7a034a39d2509d99eac99c15
Vitor Santos <vitor93gs@gmail.com>
2022-03-11 09:48:16 -0300

    outra tentativa de bugfix, hard coding o endereço da DB

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 module.exports =  async function connect(){
     try {
-        const dbConnection = await mongoose.connect(process.env.mongoDB_URI)
+        const dbConnection = await mongoose.connect("mongodb+srv://vitorsantos:XtMuctDg3GPgG6VN@diet-based-recipes-db.hjbtp.mongodb.net/diet-based-recipes-db?retryWrites=true")
         console.log("Connected to DB: ", dbConnection.connection.name)
     } catch (error) {
         console.error("DB connection error ", error)
